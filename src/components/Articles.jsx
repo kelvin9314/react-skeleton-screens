c72608e82be630a34c195d7f534acd9624b8b807
kelvin9314 <kelvin9314@gmail.com>
2020-11-16 01:06:17 +0800

    Fetching Data

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,32 @@
-const Articles = () =>{
+import { useState, useEffect } from 'react'
+const Articles = () => {
+  const [articles, setArticles] = useState(null)
+
+  const delayRequestForSkeleton = () => {
+    setTimeout(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const data = await res.json()
+      console.log(data)
+      setArticles(data)
+    }, 5000)
+  }
+
+  useEffect(() => {
+    delayRequestForSkeleton()
+  }, [])
+
   return (
     <div className="articles">
       <h2> Articles </h2>
+
+      {articles &&
+        articles.map(article => (
+          <div className="article" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+      {!articles && <div>Loading...</div>}
     </div>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react'
+
 const User = () => {
+  const [profile, setProfile] = useState(null)
+
+  const delayRequestForSkeleton = () => {
+    setTimeout(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
+      const data = await res.json()
+      console.log(data)
+      setProfile({ ...data })
+    }, 5000)
+  }
+
+  useEffect(() => {
+    delayRequestForSkeleton()
+  }, [])
   return (
     <div className="user">
       <h2> User Details </h2>
+
+      {profile && (
+        <div className="profile">
+          <h3> {profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+
+      {!profile && <div>Loading...</div>}
     </div>
   )
 }
